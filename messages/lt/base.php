cd859ec5690d6888b1bfc569103f475eabaf5003
Lucas Bartholemy <lucas@bartholemy.com>
2015-06-07 21:24:37 +0200

    Added Lithuanian language files

---
+++ messages/lt/base.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  'Manage <strong>reported posts</strong>' => '',
+  'Reported posts' => '',
+  'by :displayName' => '',
+);
---
+++ messages/lt/views_notifications_newReport.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  'An user has reported your post as offensive.' => '',
+  'An user has reported your post as spam.' => '',
+  'An user has reported your post for not belonging to the space.' => '',
+);
