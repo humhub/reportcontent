ca6ef87ea90249f33dc221fb0208634eeb51fce8
Lucas Bartholemy <lucas@bartholemy.com>
2015-07-08 22:38:27 +0200

    Added Croatian language files

---
+++ messages/hr/views_notifications_newReportAdmin.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  '%displayName% has reported %contentTitle% as offensive.' => '',
+  '%displayName% has reported %contentTitle% as spam.' => '',
+  '%displayName% has reported %contentTitle% for not belonging to the space.' => '',
+);
