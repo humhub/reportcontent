73a2f9ec28ef3dc76acb24aca5ef6213efaa8ad0
HumHub Translate <info@humhub.com>
2016-04-03 15:49:01 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/tr/base.php
+++ messages/tr/base.php
@@ -3,6 +3,6 @@ return array (
   'Manage <strong>reported posts</strong>' => 'Yönet <strong>bildirilen mesajlar</strong>',
   'Reported posts' => 'Raporlar',
   'Why do you want to report this post?' => '',
-  'by :displayName' => 'tarafından :displayName',
-  'created by :displayName' => '',
+  'by :displayName' => ':displayName tarafından',
+  'created by :displayName' => 'oluşturan :displayName',
 );
