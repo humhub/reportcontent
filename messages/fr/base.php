dc2ef8af4ebad9b13841ada4d4718de90e825fae
HumHub Translate <info@humhub.com>
2016-05-24 15:24:50 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/base.php
+++ messages/fr/base.php
@@ -1,8 +1,8 @@
 <?php
 return array (
-  'Manage <strong>reported posts</strong>' => '',
-  'Reported posts' => '',
-  'Why do you want to report this post?' => '',
+  'Manage <strong>reported posts</strong>' => 'Gérer les <strong>contenus signalés</strong>',
+  'Reported posts' => 'Contenus signalés',
+  'Why do you want to report this post?' => 'Pourquoi voulez-vous signaler ce contenu ?',
   'by :displayName' => 'par :displayName',
   'created by :displayName' => 'créé par :displayName',
 );
--- messages/fr/widgets_views_reportSpamLink.php
+++ messages/fr/widgets_views_reportSpamLink.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  'Does not belong to this space' => '',
-  'Help Us Understand What\'s Happening' => '',
-  'It\'s offensive' => '',
-  'It\'s spam' => '',
-  'Report post' => '',
+  'Does not belong to this space' => 'Ne concerne pas cet espace.',
+  'Help Us Understand What\'s Happening' => 'Aidez-nous à comprendre ce qui se passe',
+  'It\'s offensive' => 'C\'est choquant',
+  'It\'s spam' => 'C\'est du spam',
+  'Report post' => 'Signaler le contenu',
   'Submit' => 'Envoyer',
 );
