f0db3fdd37a7caf289d7767764d6f3f0b8a5f57f
HumHub Translate <info@humhub.com>
2016-07-08 14:03:37 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/tr/widgets_views_reportSpamLink.php
+++ messages/tr/widgets_views_reportSpamLink.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Does not belong to this space' => 'Bu mekana ait değil',
-  'Help Us Understand What\'s Happening' => '',
+  'Help Us Understand What\'s Happening' => 'Ne Olduğunu Anlamamıza Yardımcı Ol',
   'It\'s offensive' => 'Saldırgan içerik',
   'It\'s spam' => 'Spam mesaj',
   'Report post' => 'Mesajı bildir',
