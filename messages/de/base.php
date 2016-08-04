58a18948491911417165e4d264e232527dd1c1a1
Carsten Brandt <mail@cebe.cc>
2016-08-04 16:29:16 +0200

    Update translation for 'Reported posts'
    This is the entry in admin Navigation and should be 'Gemeldete Beiträge'.

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Manage <strong>reported posts</strong>' => 'Bearbeite <strong>gemeldete Beiträge</strong>',
-  'Reported posts' => 'Melde den Beitrag',
+  'Reported posts' => 'Gemeldete Beiträge',
   'Why do you want to report this post?' => 'Warum möchtest Du den Beitrag melden?',
   'by :displayName' => 'von :displayName',
   'created by :displayName' => 'erstellt von :displayName',
