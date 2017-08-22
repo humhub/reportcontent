f3733ecb3138afb9ec280c1cef4a7f8f498f0179
Sergio <sergioguillot@gmail.com>
2017-08-22 21:47:00 +0200

    Update widgets_views_reportContentAdminGrid.php
    Translation completed

--- messages/es/widgets_views_reportContentAdminGrid.php
+++ messages/es/widgets_views_reportContentAdminGrid.php
@@ -1,16 +1,16 @@
 <?php
 return array (
   '<strong>Confirm</strong> post deletion' => '<strong>Confirma</strong> el borrado de la entrada',
-  '<strong>Confirm</strong> report deletion' => '',
-  'Approve' => '',
-  'Approve post' => '',
+  '<strong>Confirm</strong> report deletion' => '<strong>Confirma</strong> borrar el reporte',
+  'Approve' => 'Aprobar',
+  'Approve post' => 'Aprobar entrada',
   'Cancel' => 'Cancelar',
   'Content' => 'Contenido',
   'Delete' => 'Borrar',
   'Delete post' => 'Eliminar entrada',
-  'Do you really want to approve this post?' => '',
+  'Do you really want to approve this post?' => '¿Quieres aprobar esta entrada?',
   'Do you really want to delete this post? All likes and comments will be lost!' => '¿Realmente deseas borrar esta entrada? ¡Todos los "Me gusta" y los comentarios se perderán!',
-  'Reason' => 'Razón',
+  'Reason' => 'Motivo',
   'Reporter' => 'Reportador',
-  'There are no reported posts.' => '',
+  'There are no reported posts.' => 'No hay entradas reportadas',
 );
