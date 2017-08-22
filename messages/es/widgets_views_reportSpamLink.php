4e3a280c21ba0cb9fec21250a6ca27abc629e6ab
Sergio <sergioguillot@gmail.com>
2017-08-22 21:53:28 +0200

    Update widgets_views_reportSpamLink.php

--- messages/es/widgets_views_reportSpamLink.php
+++ messages/es/widgets_views_reportSpamLink.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  'Does not belong here' => '',
-  'Help Us Understand What\'s Happening' => '',
-  'It\'s offensive' => '',
-  'It\'s spam' => '',
-  'Report post' => '',
+  'Does not belong here' => 'No corresponde aquí',
+  'Help Us Understand What\'s Happening' => 'Ayúdanos a saber qué está pasando',
+  'It\'s offensive' => 'Es ofensivo',
+  'It\'s spam' => 'Es spam',
+  'Report post' => 'Reportar entrada',
   'Submit' => 'Enviar',
 );
