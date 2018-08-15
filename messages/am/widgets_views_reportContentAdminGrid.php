9f52ff48bd3a77fbcebe6c07cba130b9c10f364f
HumHub Translations <info@humhub.com>
2018-08-15 11:53:57 +0200

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/am/widgets_views_reportContentAdminGrid.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    '<strong>Confirm</strong> post deletion' => '',
+    '<strong>Confirm</strong> report deletion' => '',
+    'Approve' => '',
+    'Approve post' => '',
+    'Cancel' => '',
+    'Content' => '',
+    'Delete' => '',
+    'Delete post' => '',
+    'Do you really want to approve this post?' => '',
+    'Do you really want to delete this post? All likes and comments will be lost!' => '',
+    'Reason' => '',
+    'Reporter' => '',
+    'There are no reported posts.' => '',
+];
