198a50074998df7a6507e7761800416ac7062fca
HumHub Translations <info@humhub.com>
2020-01-30 15:35:53 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/ro/widgets_views_reportContentAdminGrid.php
+++ messages/ro/widgets_views_reportContentAdminGrid.php
@@ -1,15 +1,15 @@
 <?php
 return array (
-  '<strong>Confirm</strong> post deletion' => '',
+  '<strong>Confirm</strong> post deletion' => '<strong>Confirmă</strong> ștergerea postării',
   '<strong>Confirm</strong> report deletion' => '',
   'Approve' => '',
   'Approve post' => '',
   'Cancel' => 'Anulează',
-  'Content' => '',
+  'Content' => 'Conținut',
   'Delete' => 'Șterge',
   'Delete post' => '',
   'Do you really want to approve this post?' => '',
-  'Do you really want to delete this post? All likes and comments will be lost!' => '',
+  'Do you really want to delete this post? All likes and comments will be lost!' => 'Sigur dorești să ștergi această postare? Toate aprecierile și comentariile vor fi pierdute!',
   'Reason' => '',
   'Reporter' => '',
   'There are no reported posts.' => '',
