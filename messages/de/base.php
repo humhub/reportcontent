f43dd8bdb1bc4e9222e374311a229f05a6930263
HumHub Translations <info@humhub.com>
2020-02-27 11:50:28 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -3,7 +3,7 @@ return array (
   'Manage <strong>reported posts</strong>' => 'Verwalte <strong>gemeldete Beiträge</strong>',
   'Please provide a reason, why you want to report this content.' => 'Bitte gib einen Grund an, warum du diesen Inhalt melden möchtest.',
   'Reported posts' => 'Gemeldete Beiträge',
-  'Why do you want to report this post?' => 'Warum möchtest Du den Beitrag melden?',
+  'Why do you want to report this post?' => 'Warum möchtest du den Beitrag melden?',
   'by :displayName' => 'von :displayName',
   'created by :displayName' => 'erstellt von :displayName',
 );
