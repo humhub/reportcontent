7801e3bc7f324b50418b6dedf3fcb3d73f8984b0
HumHub Translations <info@humhub.com>
2020-12-18 16:24:56 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hr/widgets_views_reportContentAdminGrid.php
+++ messages/hr/widgets_views_reportContentAdminGrid.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Approve' => 'Odobri',
-    'Cancel' => 'Poništi',
-    'Content' => 'Sadržaj',
-    'Do you really want to approve this post?' => 'Zaista želite odobriti ovu objavu?',
-    'Reason' => 'Razlog',
-    'Reporter' => 'Prijavitelj',
-    'There are no reported posts.' => 'Nema prijavljenih objava.',
-    '<strong>Approve</strong> content' => '',
-    'Review' => '',
-];
+return array (
+  '<strong>Approve</strong> content' => '<strong>Approve</strong> content',
+  'Approve' => 'Odobri',
+  'Cancel' => 'Poništi',
+  'Content' => 'Sadržaj',
+  'Do you really want to approve this post?' => 'Zaista želite odobriti ovu objavu?',
+  'Reason' => 'Razlog',
+  'Reporter' => 'Prijavitelj',
+  'Review' => 'Pregled',
+  'There are no reported posts.' => 'Nema prijavljenih objava.',
+);
