84efbd802f62b9ba3ebcf396a8bc2eae7d9ab909
HumHub Translations <info@humhub.com>
2021-02-04 15:33:46 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/widgets_views_reportContentAdminGrid.php
+++ messages/de/widgets_views_reportContentAdminGrid.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Approve' => 'Genehmigen',
-    'Cancel' => 'Abbrechen',
-    'Content' => 'Inhalt',
-    'Do you really want to approve this post?' => 'Möchtest du diesen Beitrag wirklich genehmigen?',
-    'Reason' => 'Grund',
-    'Reporter' => 'Melder',
-    'There are no reported posts.' => 'Keine gemeldeten Beiträge.',
-    '<strong>Approve</strong> content' => '',
-    'Review' => '',
-];
+return array (
+  '<strong>Approve</strong> content' => '<strong>Inhalt</strong> freigeben',
+  'Approve' => 'Genehmigen',
+  'Cancel' => 'Abbrechen',
+  'Content' => 'Inhalt',
+  'Do you really want to approve this post?' => 'Möchtest du diesen Beitrag wirklich genehmigen?',
+  'Reason' => 'Grund',
+  'Reporter' => 'Melder',
+  'Review' => 'Überprüfen',
+  'There are no reported posts.' => 'Keine gemeldeten Beiträge.',
+);
