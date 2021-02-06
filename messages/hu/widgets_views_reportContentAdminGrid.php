8828832cc5fcbbea9aa20aabf33abc0e3d68318f
HumHub Translations <info@humhub.com>
2021-02-06 18:31:36 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/widgets_views_reportContentAdminGrid.php
+++ messages/hu/widgets_views_reportContentAdminGrid.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Approve' => 'Elfogadás',
-    'Cancel' => 'Mégsem',
-    'Content' => 'Tartalom',
-    'Do you really want to approve this post?' => 'Tényleg jóváhagyod ezt a bejegyzést?',
-    'Reason' => 'Ok',
-    'Reporter' => 'Bejelentő',
-    'There are no reported posts.' => 'Nincsenek jelentett bejegyzések.',
-    '<strong>Approve</strong> content' => '',
-    'Review' => '',
-];
+return array (
+  '<strong>Approve</strong> content' => 'Tartalom <strong>Jóváhagyása</strong>',
+  'Approve' => 'Elfogadás',
+  'Cancel' => 'Mégsem',
+  'Content' => 'Tartalom',
+  'Do you really want to approve this post?' => 'Tényleg jóváhagyod ezt a bejegyzést?',
+  'Reason' => 'Ok',
+  'Reporter' => 'Bejelentő',
+  'Review' => 'Előnézet',
+  'There are no reported posts.' => 'Nincsenek jelentett bejegyzések.',
+);
