51a897692c94721aed5f211de951ec1898215bf8
HumHub Translations <hello@humhub.com>
2022-09-06 16:32:57 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,9 +1,8 @@
 <?php
-
-return [
-    'Manage <strong>reported posts</strong>' => '',
-    'Please provide a reason, why you want to report this content.' => '',
-    'Reported posts' => '',
-    'Why do you want to report this post?' => '',
-    'created by :displayName' => '',
-];
+return array (
+  'Manage <strong>reported posts</strong>' => 'Gerir <strong>publicações denunciadas</strong>',
+  'Please provide a reason, why you want to report this content.' => 'Por favor, indique uma razão pela qual quer denunciar este conteúdo.',
+  'Reported posts' => 'Publicações denunciadas',
+  'Why do you want to report this post?' => 'Porque deseja denunciar esta publicação?',
+  'created by :displayName' => 'criado por :displayName',
+);
