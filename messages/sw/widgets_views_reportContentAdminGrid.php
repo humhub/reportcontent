debb238e5d172021a99a61bda9f5dcc2828a36f3
HumHub Translations <hello@humhub.com>
2022-11-10 19:28:13 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/sw/widgets_views_reportContentAdminGrid.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    '<strong>Approve</strong> content' => '',
+    'Approve' => '',
+    'Cancel' => '',
+    'Content' => '',
+    'Do you really want to approve this post?' => '',
+    'Reason' => '',
+    'Reporter' => '',
+    'Review' => '',
+    'There are no reported posts.' => '',
+];
