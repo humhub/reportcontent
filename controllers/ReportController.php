a1ab8b256b89f9899e451087e590236fe4eddb2e
Lucas Bartholemy <lucas@bartholemy.com>
2023-02-24 12:07:11 +0100

    Added outdated method

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -2,13 +2,10 @@
 
 namespace humhub\modules\reportcontent\controllers;
 
-use humhub\modules\content\permissions\ManageContent;
-use humhub\modules\reportcontent\widgets\ReportContentModal;
 use humhub\widgets\ModalClose;
 use Yii;
 use yii\helpers\Url;
 use humhub\modules\reportcontent\models\ReportContent;
-use humhub\modules\content\models\Content;
 
 class ReportController extends \humhub\components\Controller
 {
@@ -56,29 +53,4 @@ class ReportController extends \humhub\components\Controller
         }
     }
 
-    public function actionDeleteContent()
-    {
-        $this->forcePostRequest();
-
-        $model = Yii::$app->request->get('model');
-        $id = Yii::$app->request->get('id');
-
-        $content = Content::get($model, $id);
-
-        $container = $content->content->getContainer();
-
-        if (Yii::$app->user->identity->isSystemAdmin() ||
-            $content->content->getContainer()->permissionManager->can(new ManageContent())) {
-            $content->delete();
-        }
-
-        if (Yii::$app->request->get('admin')) {
-            return $this->htmlRedirect(Url::to(['/reportcontent/admin']));
-        } else {
-            return $this->htmlRedirect($container->createUrl('/reportcontent/space-admin'));
-        }
-    }
-
 }
-
-?>
